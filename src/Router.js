362bef9d4a7cae0f25b560a71b4c6711a0e06257
JankMajesty <lmcgill36@gmail.com>
2021-10-03 15:02:54 -0500

    final push

--- src/Router.js
+++ src/Router.js
@@ -6,19 +6,33 @@ import About from './components/About'
 import Car from './components/Car'
 import Login from './components/Login'
 
+console.clear()
+
 // Write checkAuth function here
 // Check the cookies for a cookie called "loggedIn"
-
+const checkAuth = () => {
+    const cookies = cookie.parse(document.cookie)
+    return cookies["loggedIn"] ? true : false
+}
 
 // Write ProtectedRoute function here
-
+const ProtectedRoute = ({component: Component, ...rest}) => {
+    return (
+      <Route
+      {...rest}
+      render={(props) => checkAuth() 
+          ? <Component {...props} />
+          : <Redirect to="/login" />}
+      />
+    );
+  };
 
 const Router = () => {
     return (
         <Switch>
             <Route path="/login" component={Login} />
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
+            <ProtectedRoute exact path="/" component={Home} />
+            <ProtectedRoute path ="/about" component = {About} />
             <Route path="/car/:id" component={Car} />
         </Switch>
     );
